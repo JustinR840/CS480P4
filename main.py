f86f0a014ca0584d0ac9b9c100efa4811c9f2753
Elias James Mote <eliasjmote@gmail.com>
2018-11-05 19:29:01 -0800

    Fixed some bugs, Eval() may not be working correctly (minimax does not always find optimal solution)

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 def main():
-	global player1Sym
-	player1Sym = '1'
-	global player2Sym
-	player2Sym = '2'
-	global blankSym
-	blankSym = '0'
-
-	# Intialize winning board one time so we don't have to recalculate it
-	calcWinPos()
-
 	# Main loop. Keep running until we break out when selection == 3 (Quit Program)
 	while(True):
 		selection = GetProblemChoice()
 
 		if(selection == 1):
-			board = DoMinMax(GetInputFile())
-			for i in range(len(board[0])):
-				print(board[0][i])
+			board, value, numNodes = DoMinMax(GetInputFile())
+			if(numNodes > 1):
+				if(value > 0):
+					print("Board value is 1")
+				elif(value < 0):
+					print("Board value is 0")
+				elif(value == 0):
+					print("Board value is 0.5")
+			elif(numNodes == 1):
+				print("Board value is " + str(value))
+			print("Number of nodes expanded is " + str(numNodes))
+			for r in board:
+				print(r)
 		elif(selection == 2):
 			DoAlphaBeta()
 		elif(selection == 3):
@@ -28,14 +28,14 @@ def main():
 def calcWinPos():
 	# There are 24 ways to win (permutations of (0, 1, 2, 3)) for both
 	# coordinates.
-	global winList
 	winList = []
-	winBoard = [[blankSym for i in range(4)] for j in range(4)]
+	winBoard = [["0" for r in range(4)] for c in range(4)]
 
 	for i in range(4):
 		for j in range(4):
 			for k in range(4):
 				for l in range(4):
+					winBoard = [["0" for r in range(4)] for c in range(4)]
 					# Pick the 1st one
 					winBoard[i][0] = "P"
 
@@ -45,14 +45,19 @@ def calcWinPos():
 					winBoard[j][1] = "P"
 
 					# Pick the 3rd one (k != j)
-					if(k == j):
+					if(k == j or k == i):
 						continue
 					winBoard[k][2] = "P"
 
 					# Pick the 4th one (l != k)
-					if(l == k):
+					if(l == k or l == j or l == i):
 						continue
-					winBoard[k][3] = "P"
+					winBoard[l][3] = "P"
+
+					winList.append(winBoard)
+					
+
+	return winList
 
 # Count the number of player 1 symbols and player 2 symbols
 def CountP1AndP2(board):
@@ -60,9 +65,9 @@ def CountP1AndP2(board):
 	p2Count = 0
 	for i in range(len(board)):
 		for j in board[i]:
-			if(j == player1Sym):
+			if(j == "1"):
 				p1Count += 1
-			elif(j == player2Sym):
+			elif(j == "2"):
 				p2Count += 1
 	return (p1Count, p2Count)
 
@@ -75,10 +80,9 @@ def CheckBoardLegality(board):
 	# First, count the number of X's and O's
 	p1Count, p2Count = CountP1AndP2(board)
 
-	# Take the absolute value of the difference of X's and O's
+	# Take the absolute value of the difference of 1's and 2's
 	# If this value is greater than 1, return -1
 	if(abs(p1Count - p2Count) > 1):
-		print("Board value: -1")
 		return -1
 
 	# Next, check if player 1 won
@@ -96,7 +100,7 @@ def CheckBoardLegality(board):
 		ColsFilled = [False] * 4
 		for i in range(len(board)):
 			for j in range(len(board[i])):
-				if(board[i][j] == player1Sym):
+				if(board[i][j] == "1"):
 					RowsFilled[i] = True
 					ColsFilled[j] = True
 
@@ -125,7 +129,7 @@ def CheckBoardLegality(board):
 		ColsFilled = [False] * 4
 		for i in range(len(board)):
 			for j in range(len(board[i])):
-				if(board[i][j] == player2Sym):
+				if(board[i][j] == "2"):
 					RowsFilled[i] = True
 					ColsFilled[j] = True
 
@@ -140,17 +144,17 @@ def CheckBoardLegality(board):
 
 	# If player 1 and player 2 have won, return -1
 	if(Player1Win and Player2Win):
-		print("Board value: -1")
+		#print("Board value: -1")
 		return -1
 
 	# If player 1 won, return 1
 	if(Player1Win):
-		print("Board value: 1")
+		#print("Board value: 1")
 		return 1
 
 	# If player 2 won, return 0
 	if(Player2Win):
-		print("Board value: 0")
+		#print("Board value: 0")
 		return 0
 
 	# Otherwise, continue as normal
@@ -180,11 +184,11 @@ def CheckPlayerVictory(board, player):
 		for i in range(len(board)):
 			for j in range(len(board[i])):
 				if(player == 1):
-					if(board[i][j] == player1Sym):
+					if(board[i][j] == "1"):
 						rowsFilled[i] = True
 						colsFilled[j] = True
 				else:
-					if(board[i][j] == player2Sym):
+					if(board[i][j] == "2"):
 						rowsFilled[i] = True
 						colsFilled[j] = True
 
@@ -201,7 +205,6 @@ def CheckPlayerVictory(board, player):
 
 # Checks if the board is a leaf node (either the board is filled or
 # one of the players has won).
-# R
 def Leaf(board):
 	# Check if each player has won. If a player has won, the board
 	# is at a leaf node
@@ -223,7 +226,6 @@ def Leaf(board):
 # The static eval is:
 # Number of ways player 1 can win - number of ways player 2 can win
 # To count the number of ways a player can win:
-# 
 def Eval(board):
 	if(CheckPlayerVictory(board, 1)):
 		return 100
@@ -240,6 +242,7 @@ def Eval(board):
 	# player as filled. At the end of each board comparison, if there are
 	# 4 matches, then that is a possible win state for the player
 	player1Eval = 0
+	winList = calcWinPos()
 	for winBoard in winList:
 		numMatches = 0
 
@@ -247,7 +250,7 @@ def Eval(board):
 		for i in range(len(board)):
 			for j in range(len(board[i])):
 				if(winBoard[i][j] == "P"):
-					if(board[i][j] == player1Sym or board[i][j] == blankSym):
+					if(board[i][j] == "1" or board[i][j] == "0"):
 						numMatches += 1
 
 		if(numMatches == 4):
@@ -261,7 +264,7 @@ def Eval(board):
 		for i in range(len(board)):
 			for j in range(len(board[i])):
 				if(winBoard[i][j] == "P"):
-					if(board[i][j] == player2Sym or board[i][j] == blankSym):
+					if(board[i][j] == "2" or board[i][j] == "0"):
 						numMatches += 1
 
 		if(numMatches == 4):
@@ -271,7 +274,7 @@ def Eval(board):
 
 # Returns true if the node is a max node, false otherwise
 # Basically, true if it is player 1's turn, false if player 2's
-# Since player 1 (X) goes first, if there are an equal number of
+# Since player 1 (1) goes first, if there are an equal number of
 # X's and O's, it is player 1's turn
 # Else, it is player 2's turn
 def MaxNode(board):
@@ -282,6 +285,7 @@ def MaxNode(board):
 
 # Return a list of all immediate successor boards for the given player
 def Succ(board, player):
+	import copy
 	succList = []
 
 	# Check the current board for empty spaces. For each empty space,
@@ -289,34 +293,38 @@ def Succ(board, player):
 	# space and append it to the successor list.
 	for i in range(len(board)):
 		for j in range(len(board[i])):
-			if(board[i][j] == blankSym):
-				succ = board.copy()
+			if(board[i][j] == "0"):
+				
+				succ = copy.deepcopy(board)
 				if(player == 1):
-					succ[i][j] = player1Sym
+					succ[i][j] = "1"
 				else:
-					succ[i][j] = player2Sym
+					succ[i][j] = "2"
 				succList.append(succ)
 
 	return succList
 
 # Recursive minimax prodecure
 def DoMinMax(u):
+	#print(u)
+	#print()
+	# Number of nodes expanded
+	numNodes = 1
 
 	# Check if the board is legal. End the function with a -1 if not legal.
 	# Also end the function if player 1 or 2 has won
 	BoardValue = CheckBoardLegality(u)
 	if(BoardValue != 2):
-		return (u, BoardValue)
+		return (u, BoardValue, numNodes)
 
 	#TODO: Output board value
 	# Output 1 if win for 1st player
 	# Output 0.5 if draw
 	# Output 0 if win for 2nd player
 
-
 	# Return the static evaluation if the current board is a leaf node
 	if(Leaf(u)):
-		return (u, Eval(u))
+		return (u, Eval(u), numNodes)
 
 	# Which player's turn is it?
 	curPlayer = -1
@@ -333,7 +341,7 @@ def DoMinMax(u):
 	else:
 		val = float("inf")
 		curPlayer = 2
-	
+
 	# Resulting board
 	res = None
 
@@ -342,19 +350,22 @@ def DoMinMax(u):
 
 		# Recursive call at MAX node
 		if(curPlayer == 1):
-			if(DoMinMax(v)[1] > val):
+			maxRes, maxVal, maxNodes = DoMinMax(v)
+			numNodes += maxNodes
+			if(maxVal > val):
 				res = v
-				val = DoMinMax(v)[1]
+				val = maxVal
 
 		# Recursive call at MIN node
 		else:
-			if(DoMinMax(v)[1] < val):
+			minRes, minVal, minNodes = DoMinMax(v)
+			numNodes += minNodes
+			if(minVal < val):
 				res = v
-				val = DoMinMax(v)[1]
+				val = minVal
 
 	# Return final evaluation
-	return (res, val)
-
+	return (res, val, numNodes)
 
 def DoAlphaBeta(u, alpha, beta):
 	"""board = GetProblemChoice()
@@ -383,13 +394,12 @@ def DoAlphaBeta(u, alpha, beta):
 
 # Ask for an input file. Read the contents of that file.
 def GetInputFile():
-	print("The input file should be in the same directory as main.py")
+	#print("The input file should be in the same directory as main.py")
 	filename = "test.input" #TODO: input("Enter the name of the input file: ")
 
 	with open(filename) as fp:
 		alltext = fp.read()
 		return InputToBoard(alltext)
-
 
 # Transform a string into a 2D array representing the gameboard.
 def InputToBoard(text):
@@ -401,7 +411,6 @@ def InputToBoard(text):
 		board[row] = list(board[row])
 
 	return board
-
 
 # Get a valid input choice from the user so we know what action to perform.
 def GetProblemChoice():
@@ -424,5 +433,5 @@ def GetProblemChoice():
 	# Convert selection to int before returning.
 	return int(selection)
 
-
-main()
+if __name__ == "__main__":
+	main()
